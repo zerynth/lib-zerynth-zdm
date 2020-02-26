7d40b5c9ad3e3351094170b8dd421e4a029ec8b9
e.Neri <enrico.neri.93@gmail.com>
2020-02-26 10:00:57 +0100

    device read changeset and download file from ADM

--- downloadfirmware.py
+++ downloadfirmware.py
@@ -1,6 +1,7 @@
 # import streams
 import streams
 import json
+from mqtt import mqtt
 
 # import the wifi interface
 from wireless import wifi
@@ -17,9 +18,15 @@ wifi_driver.auto_init()
 
 wifi_name = "Zerynth"
 wifi_password = "zerynthwifi"
-fw_download_link = "http://api.adm.zerinth.com/v1/workspace/wks-4pc4a2v05zpd/firmware/firm4pc4g0ex5nnm/download"
+# fw_download_link = "http://api.adm.zerinth.com/v1/workspace/wks-4pc4a2v05zpd/firmware/firm4pc4g0ex5nnm/download"
 auth = {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmciOiIiLCJpYXQiOjE1ODIxOTM0NTAsImlzcyI6InplcnludGgiLCJleHAiOjE1ODQ3ODU0NTAsInVpZCI6ImZfQUpld3VrVGZhd1U3VUhKMHBJQmciLCJqdGkiOiJpZjZpNTJ5OVN4Q09TSUVnQWJ6V3R3IiwibHRwIjpudWxsfQ.w6Wbhf81K4OHv_N07mElscPp979feF4wE0LpNZGYqYU"}
 
+device_id = "dev-4pcidr47kutt"
+client_id = "dev-4pcidr47kutt"
+password  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkZXYtNHBjaWRyNDdrdXR0IiwidXNlciI6ImRldi00cGNpZHI0N2t1dHQiLCJleHAiOjE5MTYyMzkwMjIsImtleSI6MX0.wcMkmJGvHLz5juaL9scUpv9PKsMWdXMh_5oCbkGckCA"
+broker    = "rmq.adm.zerinth.com"
+
+topic = "j/dn/" + device_id
 # use the wifi interface to link to the Access Point
 # change network name, security and password as needed
 print("Establishing Link...")
@@ -31,25 +38,52 @@ except Exception as e:
         sleep(1000)
 
 
-## let's try to connect to api.zerinth.com to download an example file
-try:
-    print("Trying to connect...")
-    # go get that time!
-    # url resolution and http protocol handling are hidden inside the requests module
-    response = requests.get(fw_download_link, headers=auth)
-    # let's check the http response status: if different than 200, something went wrong
-    print("Http Status:",response.status)
-    # if we get here, there has been no exception, exit the loop
-except Exception as e:
-    print(e)
+# define MQTT callbacks
+def is_fota(data):
+    message = data['message']
+    body = json.loads(message.payload)
+    if body["key"] == "fota":
+        return True
+    return False
 
+def process_fota(client,data):
+    try:
+        message = data['message']
+        body = json.loads(message.payload)
+        value = (body["value"])
+        fw_download_link = value["url"]
+        ## connect to api.adm.zerinth.com to download the firmware
+        print("Trying to connect...")
+        response = requests.get(fw_download_link, headers=auth)
+        # let's check the http response status: if different than 200, something went wrong
+        print("Http Status:",response.status)
+        # check status and print the result
+        if response.status==200:
+            print("Firmware downloaded successfully.")
+            print("-------------")
+            print("Version: ", value["version"])
+            print("-------------")
+            print("File content:")
+            print(response.content)
+            print("-------------")
+    except Exception as e:
+        print("An error occurred: ",e)
 
 try:
-    # check status and print the result
-    if response.status==200:
-        print("Success!!")
-        print("-------------")
-        print("And the result is:",response.content)
-        print("-------------")
+    client = mqtt.Client(client_id, True)
+    client.set_username_pw(device_id, password)
+    for retry in range(10):
+        try:
+            client.connect(broker, 60)
+            break
+        except Exception as e:
+            print("connecting...")
+    print("connected.")
+    # subscribe to channels
+    client.subscribe([[topic, 1]])
+    print("Subscribed to topic: ", topic)
+    client.on(mqtt.PUBLISH, process_fota, is_fota)
+    client.loop()
+
 except Exception as e:
-    print("ooops, something very wrong! :(",e)
+    print("an error occurred ",e)
